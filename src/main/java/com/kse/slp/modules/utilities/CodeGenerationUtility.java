8976b003fd0d0fae879afc31d9e725bc22ce8483
dungkhmt <dungkhmt@gmail.com>
2016-08-05 09:56:58 +0700

    [dungkhmt][slp]

---
+++ src/main/java/com/kse/slp/modules/utilities/CodeGenerationUtility.java
@@ -0,0 +1,13 @@
+package com.kse.slp.modules.utilities;
+
+public class CodeGenerationUtility {
+	
+	public static String genOrderCode(int id){
+		String s = "" + id;
+		while(s.length() <= 6)
+			s = "0" + s;
+		return s;
+	}
+	
+	
+}
