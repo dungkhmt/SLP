6093e5ed3d81ad99f27f7081deeafd2f1d1b3cbe
anhtu95 <anhtu23795@gmail.com>
2016-10-22 10:53:10 +0700

    [anhtu][slp.mapmodule]

--- src/main/java/com/kse/slp/modules/mapstreetmanipulation/controller/MapStreetManipulationControler.java
+++ src/main/java/com/kse/slp/modules/mapstreetmanipulation/controller/MapStreetManipulationControler.java
@@ -1,12 +1,16 @@
 package com.kse.slp.modules.mapstreetmanipulation.controller;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kse.slp.controller.BaseWeb;
+import com.kse.slp.modules.mapstreetmanipulation.model.Province;
 import com.kse.slp.modules.mapstreetmanipulation.service.ProvinceService;
 import com.kse.slp.modules.mapstreetmanipulation.service.RoadsService;
 
@@ -22,7 +26,10 @@ public class MapStreetManipulationControler extends BaseWeb {
 	ProvinceService ProvinceService;
 	
 	@RequestMapping(value="/editPoint",method=RequestMethod.GET)
-	public String editPoint(){
+	public String editPoint(ModelMap model){
+		List<Province> lstProvinces = ProvinceService.getListProvince();
+		model.put("lstProvinces", lstProvinces);
 		
+		return "mapstreetmanipulation.editPoint";
 	}
 }
