862eebca8dcad976f3c07338b774285f19cf9d5d
thuthu212810 <hongthu.sctn@gmail.com>
2017-05-06 15:42:47 +0700

    [thuthu][shipping managerment]

--- src/main/java/com/kse/slp/modules/onlinestores/modules/shippingmanagement/service/mShippersService.java
+++ src/main/java/com/kse/slp/modules/onlinestores/modules/shippingmanagement/service/mShippersService.java
@@ -10,4 +10,12 @@ public interface mShippersService {
 	public void updateShipperCurrentLocation(double lat,double lng,String user,String lastDateTime);
 	public List<mShippers> getByCustomerCode(String cus_code);
 	public List<mShippers> getListInBatch(String batchCode);
+	public int save(String shpCode, String shpDepotAd, float shpDepLat, float shpLng,
+			String shpCurrentLocation, String shpLastDate,
+			String shpStatusCode, String shpVehicle, int shpCapacity1,
+			int shpCapacity2, String shpCusCode, String shpUser);
+	public int save(mShippers ship);
+	public mShippers getByCode(String SHP_Code);
+	public int editAShipper(mShippers ship);
+	public int delAShipper(String shipCode);
 }
